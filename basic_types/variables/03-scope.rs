259f2aa04996133e381662199f375ca63e8d1475
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-05-16 21:43:42 +0200

    I ADDED #basic_types/variables/03-scope.rs

---
+++ basic_types/variables/03-scope.rs
@@ -0,0 +1,11 @@
+fn main(){
+    //A scope is the range within the program for which the iterm is valid
+    let x: i32 = 10;
+    {
+        let y:i32 = 5;
+        println!("The value of x is  {} and the value of y is {}", x, y);
+    }// This is the inner scope
+
+    println!("the value of x is {} and the value of y is {}", x, y)
+
+}//This is the outer scope
